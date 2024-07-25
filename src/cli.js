bba5d99a8135deb60b3d2029fa474a68c0da624e
Андрей Сахаров <saxared15@gmail.com>
2024-07-25 05:29:30 -0300

    fix welcome message

--- src/cli.js
+++ src/cli.js
@@ -1,3 +1,6 @@
 import readlineSync from 'readline-sync';
 
-export default readlineSync.question('May I have your name? ');
+export default () => {
+let name = readlineSync.question('May I have your name? ');
+console.log(`Hello, ${name}`);
+};
