6cd5981e1b9d579b2151ea357820e03b3de08a32
Андрей Сахаров <saxared15@gmail.com>
2024-08-06 02:46:33 -0300

    remade game logic in brain-calc (recomendation codeclimate)

--- src/games/gameCalc.js
+++ src/games/gameCalc.js
@@ -11,17 +11,21 @@ const brainCalc = () => {
     let randomSymbol = getRandomSymbol();
     let correctAnswer = 0;
     let question = `${randomNumbers[0]} ${randomSymbol} ${randomNumbers[1]}`;
-    if (randomSymbol === '+') {
-        correctAnswer = randomNumbers[0] + randomNumbers[1];
-    } else if (randomSymbol === '-') {
-        if (randomNumbers[0] > randomNumbers[1]) {
-            correctAnswer = randomNumbers[0] - randomNumbers[1];
-        } else {
-            question = `${randomNumbers[1]} ${randomSymbol} ${randomNumbers[0]}`;
-            correctAnswer = randomNumbers[1] - randomNumbers[0];
-        }
-    } else {
-        correctAnswer = randomNumbers[0] * randomNumbers[1];
+    switch (randomSymbol) {
+        case '*':
+            correctAnswer = randomNumbers[0] * randomNumbers[1];
+            break;
+        case '+':
+            correctAnswer = randomNumbers[0] + randomNumbers[1];
+            break;
+        case '-':
+            if (randomNumbers[0] > randomNumbers[1]) {
+                correctAnswer = randomNumbers[0] - randomNumbers[1];
+            } else {
+                question = `${randomNumbers[1]} ${randomSymbol} ${randomNumbers[0]}`;
+                correctAnswer = randomNumbers[1] - randomNumbers[0];
+            }
+            break;
     }
     console.log(`Question: ${question}`);
     return correctAnswer.toString();
